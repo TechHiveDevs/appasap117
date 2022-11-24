9efff1773db456805c083ee3c7e1793e104ac04b
TechHiveDevs <techhivedevs@gmail.com>
2022-11-24 15:52:03 +0000

    inital auto commit

---
+++ apps/client/entities/entites.ts
@@ -0,0 +1,10 @@
+
+import User from "./user/ui.user";
+import Posts from "./posts/ui.posts";
+import Follower from "./follower/ui.follower";
+
+export default [
+User,
+Posts,
+Follower,
+]
---
+++ apps/mobile/src/Config/config.ts
@@ -0,0 +1,5 @@
+
+const config = {
+  API_URL: "http://15.237.126.134:3238",
+};
+export default config;
